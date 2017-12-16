963ea7dd53452034bd44e230e8fcd0695484e284
Aarni Koskela <akx@iki.fi>
2017-12-16 14:30:49 +0200

    wop

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'env': {
+    'browser': true,
+  },
+  'rules': {
+    'one-var': 'off',
+    'object-curly-newline': 'off',
+    'no-param-reassign': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 import './style.css';
 
 import m from 'mithril';
-import TwoPole from './lib/twopole';
+
+import BiQuad from './lib/biquad';
+import { calculate, Types } from './lib/biquad-calc';
 
 const ac = new AudioContext();
 
@@ -9,38 +12,44 @@ const SAMPLE_RATE = ac.sampleRate;
 
 const oscs = [
   {
-    freq: 440,
-    freqMod: -.0019,
-    pos: 0,
-    amp: 1,
+    // type: 'sqr',
+    freq: 490,
+    // freqMod: 0.0159,
+    amp: 0.8,
     ampMod: -0.00003,
     fm: {
       freq: 250,
-      pos: 0,
-      amp: 100,
-      ampMod: 0.001,
+      amp: 550,
+      // ampMod: 0.01,
+      freqMod: 0.01,
     },
     rm: {
-      freq: 101,
-      pos: 0,
+      freq: 513,
       amp: 1,
       ampMod: 0,
     },
-    filter: {
-      twopole: null,
-      freq: 80,
-      res: 0,
+    xfilter: {
+      type: Types.LP,
+      freq: 90,
+      freqMod: 0.03,
+      res: 0.4,
     },
   },
 ];
 
 function tickFilter(value, filter) {
   if (!filter.freq) return value;
-  if (!filter.twopole) {
-    filter.twopole = new TwoPole(SAMPLE_RATE);
-    filter.twopole.setParameters(filter.freq, filter.res, true);
+
+  if (!filter.obj) {
+    filter.obj = new BiQuad(SAMPLE_RATE);
   }
-  return filter.twopole.tick(value);
+
+  filter.freq += (filter.freqMod || 0);
+
+  const params =
+      calculate(filter.type, filter.freq, filter.obj.sampleRate, filter.res, 0);
+  filter.obj.setCoefficients(params.a0, params.a1, params.a2, params.b1, params.b2, false);
+  return filter.obj.tick(value);
   /*
   filter.pos = filter.pos - (filter.beta * (filter.pos - value));
   if (filter.betaMod) filter.beta += filter.betaMod;
@@ -51,8 +60,17 @@ function tickFilter(value, filter) {
 function tickOsc(osc) {
   if (!osc) return 0;
   if (osc.amp <= 0) return 0;
-  let value = Math.sin(osc.pos * Math.PI * 2) * osc.amp;
-  let actualFreq = osc.freq + tickOsc(osc.fm);
+  if (!osc.pos) osc.pos = 0;
+  let value = 0;
+  switch (osc.type) {
+    case 'sqr':
+      value = (osc.pos % 1) < 0.5 ? -1 : 1;
+      break;
+    default:
+      value = Math.sin(osc.pos * Math.PI * 2) * osc.amp;
+      break;
+  }
+  const actualFreq = osc.freq + tickOsc(osc.fm);
   osc.pos += actualFreq / SAMPLE_RATE;
   osc.amp += osc.ampMod || 0;
   osc.freq += osc.freqMod || 0;
@@ -69,9 +87,11 @@ function render() {
   return Math.max(-1, Math.min(1, value));
 }
 
-const buffer = ac.createBuffer(1, SAMPLE_RATE, SAMPLE_RATE);
+const length = 1;
+const nFrames = Math.ceil(length * SAMPLE_RATE);
+const buffer = ac.createBuffer(1, nFrames, SAMPLE_RATE);
 const data = buffer.getChannelData(0);
-for (var i = 0; i < data.length; i++) {
+for (let i = 0; i < data.length; i++) {
   data[i] = render();
 }
 
@@ -85,23 +105,23 @@ function play(buffer) {
 
 const mapStep = (arr, step, fn) => {
   const out = [];
-  for (var i = 0; i < arr.length; i += step) {
+  for (let i = 0; i < arr.length; i += step) {
     out.push(fn(arr[i], i));
   }
   return out;
 };
 
-const view = () => {
-  return m('svg', {viewBox: '0 0 800 200'}, m('polyline', {
-             fill: 'none',
-             stroke: 'black',
-             points: mapStep(
-                         data, 1,
-                         (y, i) => `${(i / data.length * 800).toFixed(2)},${
-                             Math.round(100 + y * 90)}`)
-                         .join(' '),
-           }))
-};
+const view = () =>
+  m('svg', { viewBox: '0 0 800 200' }, m('polyline', {
+    fill: 'none',
+    stroke: 'black',
+    points: mapStep(
+      data, 1,
+      (y, i) => `${(i / data.length * 800).toFixed(2)},${
+        Math.round(100 + y * 90)}`
+)
+      .join(' '),
+  }));
 
-m.mount(document.body, {view});
+m.mount(document.body, { view });
 play(buffer);
