36f818d7790c87a8bfb1c6b5fc9bd63ada4b2d87
Aarni Koskela <akx@iki.fi>
2017-12-16 17:27:52 +0200

    mmop

--- index.js
+++ index.js
@@ -3,97 +3,59 @@ import './style.css';
 
 import m from 'mithril';
 
-import BiQuad from './lib/biquad';
-import { calculate, Types } from './lib/biquad-calc';
+import { Types } from './lib/biquad-calc';
+import getRandomValue from './lib/get-random-value';
+import MeluSynth from './lib/synth';
 
 const ac = new AudioContext();
 
 const SAMPLE_RATE = ac.sampleRate;
 
-const oscs = [
-  {
+function random(a, b) {
+  return a + (Math.random() * (b - a));
+}
+
+function genOsc() {
+  const rm =
+      (Math.random() < 0.1 ? {
+        freq: random(50, 500),
+        amp: random(0, 1),
+        ampMod: random(-0.0003, 0.0003),
+      } :
+        null);
+  const fm =
+      (Math.random() < 0.1 ? {
+        freq: random(50, 100),
+        amp: random(50, 1000),
+        // ampMod: 0.01,
+        freqMod: random(-0.01, 0.01),
+      } :
+        null);
+  const filter =
+      (Math.random() < 0.7 ? {
+        type: getRandomValue(Types),
+        freq: random(100, 5000),
+        freqMod: 0.03,
+        res: random(0.01, 0.8),
+      } :
+        null);
+  return {
     // type: 'sqr',
-    freq: 490,
+    freq: random(100, 1000),
     // freqMod: 0.0159,
-    amp: 0.8,
-    ampMod: -0.00003,
-    fm: {
-      freq: 250,
-      amp: 550,
-      // ampMod: 0.01,
-      freqMod: 0.01,
-    },
-    rm: {
-      freq: 513,
-      amp: 1,
-      ampMod: 0,
-    },
-    xfilter: {
-      type: Types.LP,
-      freq: 90,
-      freqMod: 0.03,
-      res: 0.4,
-    },
-  },
-];
-
-function tickFilter(value, filter) {
-  if (!filter.freq) return value;
-
-  if (!filter.obj) {
-    filter.obj = new BiQuad(SAMPLE_RATE);
-  }
-
-  filter.freq += (filter.freqMod || 0);
-
-  const params =
-      calculate(filter.type, filter.freq, filter.obj.sampleRate, filter.res, 0);
-  filter.obj.setCoefficients(params.a0, params.a1, params.a2, params.b1, params.b2, false);
-  return filter.obj.tick(value);
-  /*
-  filter.pos = filter.pos - (filter.beta * (filter.pos - value));
-  if (filter.betaMod) filter.beta += filter.betaMod;
-  return filter.pos;
-  */
+    amp: random(0.7, 1),
+    ampMod: random(-0.00003, 0.00003),
+    fm,
+    rm,
+    filter,
+  };
 }
 
-function tickOsc(osc) {
-  if (!osc) return 0;
-  if (osc.amp <= 0) return 0;
-  if (!osc.pos) osc.pos = 0;
-  let value = 0;
-  switch (osc.type) {
-    case 'sqr':
-      value = (osc.pos % 1) < 0.5 ? -1 : 1;
-      break;
-    default:
-      value = Math.sin(osc.pos * Math.PI * 2) * osc.amp;
-      break;
-  }
-  const actualFreq = osc.freq + tickOsc(osc.fm);
-  osc.pos += actualFreq / SAMPLE_RATE;
-  osc.amp += osc.ampMod || 0;
-  osc.freq += osc.freqMod || 0;
-  if (osc.rm) value *= tickOsc(osc.rm);
-  if (osc.filter) value = tickFilter(value, osc.filter);
-  return value;
-}
-
-function render() {
-  let value = 0;
-  oscs.forEach((osc) => {
-    value += tickOsc(osc);
-  });
-  return Math.max(-1, Math.min(1, value));
-}
-
-const length = 1;
-const nFrames = Math.ceil(length * SAMPLE_RATE);
-const buffer = ac.createBuffer(1, nFrames, SAMPLE_RATE);
+const syn = new MeluSynth(SAMPLE_RATE);
+syn.oscs.push(genOsc());
+const buffer = ac.createBuffer(1, SAMPLE_RATE * 0.7, SAMPLE_RATE);
 const data = buffer.getChannelData(0);
-for (let i = 0; i < data.length; i++) {
-  data[i] = render();
-}
+syn.render(data);
 
 function play(buffer) {
   const bs = ac.createBufferSource();
